7676ed6bcc35dcf6099688f94ee85b0687c3ad82
Jake <jacobtlanghel@gmail.com>
2022-06-07 22:41:21 -0400

    init

--- canvas.js
+++ canvas.js
@@ -1,27 +1,54 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+const animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(animate);
+};
+
+class Particle {
+  constructor() {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    this.size = Math.random * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  draw() {
+    ctx.fillStyle = "green";
+    ctx.beginPath();
+    ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
 
 window.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
 
-const drawCircle = () => {
-  ctx.fillStyle = "green";
-  ctx.beginPath();
-  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
-  ctx.fill();
-};
-
-const mouse = {
-  x: null,
-  y: null,
-};
-
 canvas.addEventListener("click", (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
-  drawCircle();
 });
+
+canvas.addEventListener("mousemove", (e) => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
+
+animate();
